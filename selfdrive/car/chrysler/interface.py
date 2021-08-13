616ad9177f5c3f7d9829ee3c554ed57f29cb5510
Drashkar <82732209+Drashkar@users.noreply.github.com>
2021-08-12 17:40:39 -0700

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -53,7 +53,7 @@ class CarInterface(CarInterfaceBase):
       #ret.lateralTuning.pid.kpBP = [0., 10., 30.]
       #ret.lateralTuning.pid.kpV = [0.04, 0.05, 0.06]
       ret.lateralTuning.pid.kpBP = [0., 10., 20., 30., 35]
-      ret.lateralTuning.pid.kpV = [0.03, 0.06, 0.065, 0.072, 0.073] #Lower for less effective steering
+      ret.lateralTuning.pid.kpV = [0.04, 0.05, 0.058, 0.068, 0.073] #Lower for less effective steering
 
       ret.lateralTuning.pid.kiBP = [0., 30.]
       ret.lateralTuning.pid.kiV = [0.03, 0.03] #Lower for less slop, higher for more slop.
