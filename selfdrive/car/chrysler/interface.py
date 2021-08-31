553b32116acf336569860e0f64b28ae685ae0857
Drashkar <82732209+Drashkar@users.noreply.github.com>
2021-08-31 07:36:16 -0700

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -53,7 +53,7 @@ class CarInterface(CarInterfaceBase):
       #ret.lateralTuning.pid.kpBP = [0., 10., 30.]
       #ret.lateralTuning.pid.kpV = [0.05, 0.06, 0.06]
       ret.lateralTuning.pid.kpBP = [0., 10., 20., 30., 35]
-      ret.lateralTuning.pid.kpV = [0.05, 0.05, 0.05, 0.07, 0.08] #Lower for less effective steering
+      ret.lateralTuning.pid.kpV = [0.04, 0.05, 0.06, 0.07, 0.08] #Lower for less effective steering
 
       #ret.lateralTuning.pid.kiBP = [0., 30.]
       #ret.lateralTuning.pid.kiV = [0.02, 0.03]
@@ -66,7 +66,7 @@ class CarInterface(CarInterfaceBase):
      # ret.lateralTuning.pid.kfBP = [0., 15., 30.]
      # ret.lateralTuning.pid.kfV = [0.000015, 0.000025, 0.000035]   # full torque for 10 deg at 80mph means 0.00007818594
       ret.lateralTuning.pid.kfBP = [0., 15., 30., 35]  #Steering torque
-      ret.lateralTuning.pid.kfV = [0.0000012, 0.000031, 0.000035, 0.000032]   # full torque for 10 deg at 80mph means 0.00007818594
+      ret.lateralTuning.pid.kfV = [0.0000010, 0.000031, 0.000035, 0.000032]   # full torque for 10 deg at 80mph means 0.00007818594
 
     ret.steerActuatorDelay = 0.4 #0.2
     ret.steerRateCost = 0.5
